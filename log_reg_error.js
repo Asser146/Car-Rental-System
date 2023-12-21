bd894118682741cb1e1a6777b68c1122c25ed1ca
Asser <assertamer2002@gmail.com>
2023-12-21 23:13:42 +0200

    Finished login/register  authentication and restrictions

--- log_reg_error.js
+++ log_reg_error.js
@@ -4,10 +4,16 @@ function getQueryParam(name) {
 }
 
 // Check if login error is set in the query parameters
-var loginError = getQueryParam('login_error');
-if (loginError === '1') {
-    alert("Incorrect password");
+var loginError1 = getQueryParam('login_error1');
+var loginError2 = getQueryParam('login_error2');
+var registerationError = getQueryParam('register_error');
+
+if (loginError1 === '1') {
+    alert("Incorrect email or pass");
 } 
-// else if (loginError === '2') {
-//     alert("No User with this email found");
-// }
+if (loginError2 === '1') {
+    alert("No User with this email found");
+}
+if(registerationError === '1'){
+    alert("This user already exists");
+}
