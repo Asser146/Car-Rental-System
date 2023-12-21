f44ef7ea8e2ca2ed10a205c97f3e73790ab03b50
Iraqy <omariraqy16@gmail.com>
2023-12-21 19:30:11 +0200

    Formatted txt to sql file , added project pdf

--- creation.sql
+++ creation.sql
@@ -1,3 +1,6 @@
+CREATE DATABASE `car rental company`;
+use `car rental company`;
+
 create table staff_user(
 staff_id int not null AUTO_INCREMENT,
 fname varchar(225) not null,
@@ -6,6 +9,7 @@ email varchar(225) not null,
 password varchar(225) not null,
 PRIMARY KEY (staff_id)
 );
+
 create table client(
 client_id int not null AUTO_INCREMENT,
 fname varchar(225) not null,
@@ -14,3 +18,4 @@ email varchar(225) not null,
 password varchar(225) not null,
 PRIMARY KEY (client_id)
 );
+
