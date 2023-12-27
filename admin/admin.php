47e78326fd1ad175fbc8608a7f67a69177435667
Asser <assertamer2002@gmail.com>
2023-12-27 22:16:13 +0200

    et5n2t

--- admin/admin.php
+++ admin/admin.php
@@ -1,204 +1,139 @@
 <?php
-global $data_base;
-$data_base = mysqli_connect("localhost", "root", "", "car rental company");
+include 'functions.php';
 
-if (!$data_base) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
-function initDB($currentDate)
-{
-    global $data_base;
-
-    $sql = "UPDATE car
-            SET car_status = 
-                CASE 
-                    WHEN car_id IN (SELECT car_id FROM reservation WHERE '$currentDate' BETWEEN reservation.start_date AND reservation.return_date) THEN 'Rented'
-                    ELSE 'Available'
-                END";
-    
-    $result = $data_base->query($sql);
-
-    if ($result === false) {
-        echo "Error updating car_status: " . $data_base->error;
-    }
-}
-
-
-
-function makeConnection() {
-    global $data_base;
-    return $data_base;
-}
-function display($result){
-    if ($result->num_rows > 0) {
-        $row = $result->fetch_assoc();
-        echo "<table border='1'>";
-        // Display headers based on column names
-        echo "<tr>";
-        foreach ($row as $key => $value) {
-            echo "<th>$key</th>";
-        }
-        echo "</tr>";
-        // Display data rows
-        $result->data_seek(0); // Reset the result set pointer
-        while ($row = $result->fetch_assoc()) {
-            echo "<tr>";
-            foreach ($row as $value) {
-                echo "<td>$value</td>";
-            }
-            echo "</tr>";
-        }
-
-        echo "</table>";
-    } else {
-        echo "No Result\n";
-    }
-}
-function insertInDb($row) {
-    global $data_base;
-    $sql = "INSERT INTO car (";
-    $sql .= implode(", ", array_keys($row));
-    $sql .= ") VALUES ('" . implode("', '", $row) . "')";
-    $result = $data_base->query($sql);
-    if ($result === false) {
-        echo "Error inserting row: " . $data_base->error;
-    } else {
-        echo "Row inserted successfully!";
-    }
-}
-
-function modifyStatus($car_id, $status) {
-    global $data_base; 
-    $sql = "SELECT * FROM car WHERE car_id = '$car_id'";
-    $result = $data_base->query($sql);
-    $row = $result->fetch_assoc();
-    $row['car_status'] = $status;
-    unset($row['car_id']);
-    $sql = "UPDATE car SET ";
-    foreach ($row as $column => $value) {
-        $sql .= "$column = '$value', ";
-    }
-    $sql = rtrim($sql, ", ");
-    $sql .= " WHERE car_id = $car_id"; 
-    $result = $data_base->query($sql);
-    if ($result === false) {
-        echo "Error updating row: " . $data_base->error;
-    } else {
-        echo "Row updated successfully!";
-    }
-}
+// Retrieve the operation type from the POST data
 $type = $_POST["type"];
-///////////////////////////////////////////////////////////////////////////////////////////////////////////
-if ($type == '1') {
-    $image_path = $_POST["image_path"];
-    $basePath = "assets\\Car Images\\";
-    $finalPath = $basePath . $image_path;
-    $row['company'] = $_POST["company_name"];
-    $row['model'] = $_POST["model_name"];
-    $row['year_made'] = $_POST["year_made"];
-    $row['image_path'] = $finalPath;
-    $row['car_status'] = 'Available';
-    $row['price_per_day'] = $_POST["price"];
-    $row['office_num'] = $_POST["office_num"];
-    insertInDb( $row); 
-}
-else if ($type == '2') {
-modifyStatus($_POST["car_id"], $_POST["status"]);
-}
-else if ($type == '3') {
-    $start = $_POST["start_date"];
-    $end = $_POST["end_date"];
-    $sql = "SELECT *
-            FROM reservation 
-            JOIN car ON reservation.car_id = car.car_id 
-            JOIN customer ON reservation.customer_id = customer.customer_id
-            WHERE reservation.start_date >= '$start' AND reservation.return_date <= '$end'";
 
+// Initialize the database connection
 global $data_base;
-$result = $data_base->query($sql);
-display($result);
-}
-else if ($type == '4') {
-    $car_id=$_POST["car_id"];
-    $start = $_POST["start_date"];
-    $end = $_POST["end_date"];
+$data_base = connectToDatabase();
 
-    $sql = "SELECT *
-    FROM reservation 
-    JOIN car ON reservation.car_id = car.car_id 
-    WHERE car.car_id = '$car_id'
-      AND DATE(reservation.start_date) >='$start'
-      AND DATE(reservation.return_date) <= '$end'";
-    global $data_base;
-    $result = $data_base->query($sql);
-    display($result);
-}
+// Process based on the operation type
+switch ($type) {
+    case '1':
+        $image_path = $_POST["image_path"];
+        $basePath = "assets\\Car Images\\";
+        $finalPath = $basePath . $image_path;
+        $row['company'] = $_POST["company_name"];
+        $row['model'] = $_POST["model_name"];
+        $row['year_made'] = $_POST["year_made"];
+        $row['image_path'] = $finalPath;
+        $row['car_status'] = 'Available';
+        $row['price_per_day'] = $_POST["price"];
+        $row['office_num'] = $_POST["office_num"];
+        insertInDb($row);
+        break;
 
-else if ($type == '5') {
-    $day = $_POST["day"];
-    initDB($day);
+    case '2':
+        modifyStatus($_POST["car_id"], $_POST["status"]);
+        break;
 
-    $sql = "SELECT  DISTINCT car.car_id, car.car_status 
-            FROM car 
-            LEFT JOIN reservation ON reservation.car_id = car.car_id";
-
-    $result = $data_base->query($sql);
-
-    if ($result) {
+    case '3':
+        $start = $_POST["start_date"];
+        $end = $_POST["end_date"];
+        $sql = "SELECT *
+                FROM reservation 
+                JOIN car ON reservation.car_id = car.car_id 
+                JOIN customer ON reservation.customer_id = customer.customer_id
+                WHERE reservation.start_date >= '$start' AND reservation.return_date <= '$end'";
+        $result = $data_base->query($sql);
         display($result);
-    } else {
-        echo "Error executing query: " . $data_base->error;
-    }
-    //Return to original date
-    $currentDate = date('Y-m-d');
-    initDB($currentDate); 
-}
+        break;
 
-else if ($type == '6') {
-    $customer_id = $_POST["customer_id"];
-    $sql = "SELECT customer.customer_id, customer.Fname, customer.Lname, customer.email, car.company, car.model, car.year_made, car.car_id
-            FROM car 
-            JOIN reservation ON reservation.car_id = car.car_id
-            JOIN customer ON reservation.customer_id = customer.customer_id
-            WHERE customer.customer_id = '$customer_id'";
+    case '4':
+        $car_id = $_POST["car_id"];
+        $start = $_POST["start_date"];
+        $end = $_POST["end_date"];
 
-    global $data_base;
-    $result = $data_base->query($sql);
-
-    if ($result === false) {
-        die("Error in SQL query: " . $data_base->error);
-    }
-
-    display($result);
-}
-
-else if ($type == '7') {
-    $start_date = $_POST["start_date"];
-    $end_date = $_POST["end_date"];
-
-    $sql = "SELECT
-                DATE(start_date) AS reservation_day,
-                SUM(price_per_day) AS total_daily_payments
-            FROM
-                reservation
-                JOIN car ON reservation.car_id = car.car_id
-            WHERE
-                start_date BETWEEN '$start_date' AND '$end_date'
-            GROUP BY
-                DATE(start_date)
-            ORDER BY
-                reservation_day";
-
-    $result = $data_base->query($sql);
-
-    if ($result) {
-        // Assuming you have a function display() to handle the result display
+        $sql = "SELECT *
+                FROM reservation 
+                JOIN car ON reservation.car_id = car.car_id 
+                WHERE car.car_id = '$car_id'
+                  AND DATE(reservation.start_date) >='$start'
+                  AND DATE(reservation.return_date) <= '$end'";
+        $result = $data_base->query($sql);
         display($result);
-    } else {
-        echo "Error executing query: " . $data_base->error;
-    }
+        break;
+
+    case '5':
+        $day = $_POST["day"];
+        initDB($day);
+
+        $sql = "SELECT DISTINCT car.car_id, car.car_status 
+                FROM car 
+                LEFT JOIN reservation ON reservation.car_id = car.car_id";
+        $result = $data_base->query($sql);
+
+        if ($result) {
+            display($result);
+        } else {
+            echo "Error executing query: " . $data_base->error;
+        }
+
+        // Return to the original date
+        $currentDate = date('Y-m-d');
+        initDB($currentDate);
+        break;
+
+    case '6':
+        $customer_id = $_POST["customer_id"];
+        $sql = "SELECT customer.customer_id, customer.Fname, customer.Lname, customer.email, car.company, car.model, car.year_made, car.car_id
+                FROM car 
+                JOIN reservation ON reservation.car_id = car.car_id
+                JOIN customer ON reservation.customer_id = customer.customer_id
+                WHERE customer.customer_id = '$customer_id'";
+        $result = $data_base->query($sql);
+
+        if ($result === false) {
+            die("Error in SQL query: " . $data_base->error);
+        }
+
+        display($result);
+        break;
+
+        case '7':
+            // $start_date = $_POST["start_date"];
+            // $end_date = $_POST["end_date"];
+            // $start = new DateTime($start_date);
+            // $end = new DateTime($end_date);
+            // $interval = new DateInterval('P1D'); // 1 day interval
+            // $dateRange = new DatePeriod($start, $interval, $end);
+        
+            // echo '<table>';
+            // echo '<tr><th>Date</th><th>Total Daily Payments</th></tr>';
+        
+            // foreach ($dateRange as $date) {
+            //     $current = $date->format('Y-m-d');
+            //     $sql="SELECT *
+            //     FROM reservation
+            //     WHERE $current BETWEEN reservation.start_date AND reservation.return_date";
+                
+
+            //     $result = $data_base->query($sql);
+        
+            //     if ($result->num_rows > 0) {
+            //         while ($row = $result->fetch_assoc()) {
+            //             echo '<tr>';
+            //             echo '<td>' . htmlspecialchars($current) . '</td>';
+            //             echo '<td>' . htmlspecialchars($row['car_id']) . '</td>';
+            //             echo '<td>' . htmlspecialchars($row['total_daily_payments']) . '</td>';
+            //             echo '</tr>';
+            //         }
+            //     } else {
+            //         // If there are no results for the current date
+            //         echo '<tr><td>' . htmlspecialchars($current) . '</td><td>No Result</td></tr>';
+            //     }
+            // }
+        
+            // echo '</table>';
+            break;
+        
+
+    default:
+        // Handle the default case or add additional cases as needed
+        break;
 }
 
-
+// Close the database connection
+$data_base->close();
 ?>
---
+++ admin/functions.php
@@ -0,0 +1,124 @@
+<?php
+function connectToDatabase() {
+    $data_base = mysqli_connect("localhost", "root", "", "car rental company");
+
+    if (!$data_base) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    return $data_base;
+}
+global $data_base;
+$data_base = connectToDatabase();
+function initDB($currentDate)
+{
+    $data_base;
+
+    $sql = "UPDATE car
+            SET car_status = 
+                CASE 
+                    WHEN car_id IN (SELECT car_id FROM reservation WHERE '$currentDate' BETWEEN reservation.start_date AND reservation.return_date) THEN 'Rented'
+                    ELSE 'Available'
+                END";
+    global $data_base;
+    $result = $data_base->query($sql);
+
+    if ($result === false) {
+        echo "Error updating car_status: " . $data_base->error;
+    }
+}
+
+function display($result) {
+?>
+<html>
+<head>
+    <style>
+        body {
+            background: linear-gradient(to right, #e1eec3,#f05053); 
+            margin: 0;
+            padding: 0;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+
+        th, td {
+            padding: 8px;
+            font-size: 20px;
+            font-weight: 600;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
+    </style>
+</head>
+<body>
+
+</body>
+</html>
+
+    <?php
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        echo '<table>';
+        // Display headers based on column names
+        echo '<tr>';
+        foreach ($row as $key => $value) {
+            echo '<th>' . htmlspecialchars($key) . '</th>';
+        }
+        echo '</tr>';
+        // Display data rows
+        $result->data_seek(0); // Reset the result set pointer
+        while ($row = $result->fetch_assoc()) {
+            echo '<tr>';
+            foreach ($row as $value) {
+                echo '<td>' . htmlspecialchars($value) . '</td>';
+            }
+            echo '</tr>';
+        }
+        echo '</table>';
+    } else {
+        echo 'No Result\n';
+    }
+
+    echo '</body>';
+    echo '</html>';
+}
+
+function insertInDb($row) {
+    global $data_base;
+    $sql = "INSERT INTO car (";
+    $sql .= implode(", ", array_keys($row));
+    $sql .= ") VALUES ('" . implode("', '", $row) . "')";
+    $result = $data_base->query($sql);
+    if ($result === false) {
+        echo "Error inserting row: " . $data_base->error;
+    } else {
+        echo "Row inserted successfully!";
+    }
+}
+
+function modifyStatus($car_id, $status) {
+    global $data_base; 
+    $sql = "SELECT * FROM car WHERE car_id = '$car_id'";
+    $result = $data_base->query($sql);
+    $row = $result->fetch_assoc();
+    $row['car_status'] = $status;
+    unset($row['car_id']);
+    $sql = "UPDATE car SET ";
+    foreach ($row as $column => $value) {
+        $sql .= "$column = '$value', ";
+    }
+    $sql = rtrim($sql, ", ");
+    $sql .= " WHERE car_id = $car_id"; 
+    $result = $data_base->query($sql);
+    if ($result === false) {
+        echo "Error updating row: " . $data_base->error;
+    } else {
+        echo "Row updated successfully!";
+    }
+}
+
+?>
